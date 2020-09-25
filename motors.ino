935758d763e7c9c78b0d0c19cd93b7313411df1f
Hegazian <38352684+Hegazian@users.noreply.github.com>
2020-09-26 00:04:36 +0200

    Add files via upload

---
+++ motors.ino
@@ -0,0 +1,32 @@
+int motorright1=6;
+int motorright2=7;
+int motorleft1=8;
+int motorleft2=9;
+void setup() {
+pinMode(6,OUTPUT);
+pinMode(7,OUTPUT);
+pinMode(8,OUTPUT);
+pinMode(9,OUTPUT);
+}
+
+void loop() {
+digitalWrite(6,HIGH);
+digitalWrite(7,LOW);
+delay(10000);
+//الموتور الايمن يتحرك الي الامام
+digitalWrite(8,LOW);
+digitalWrite(9,HIGH);
+delay(1000);
+
+//الموتور الايسر يتحرك للخلف
+digitalWrite(6,LOW);
+digitalWrite(7,HIGH);
+delay(10000);
+
+//الموتور الايمن يتحرك للخلف
+digitalWrite(8,HIGH);
+digitalWrite(9,LOW);
+delay(10000);
+
+//الموتور الايسر يتحرك للامام
+}
